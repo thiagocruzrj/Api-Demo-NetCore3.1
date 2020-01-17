10f987acd2f83d6c4d5224d1407b479e5084ca5b
Thiago Cruz <thiago.justo@al.infnet.edu.br>
2020-01-17 18:20:40 -0300

    Implementing notifier

--- src/Demo.Business/Notifications/Notification.cs
+++ src/Demo.Business/Notifications/Notification.cs
@@ -4,7 +4,13 @@ using System.Text;
 
 namespace Demo.Business.Notifications
 {
-    class Notification
+    public class Notification
     {
+        public Notification(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; set; }
     }
 }
--- src/Demo.Business/Notifications/Notifier.cs
+++ src/Demo.Business/Notifications/Notifier.cs
@@ -1,10 +1,33 @@
-﻿using System;
+﻿using Demo.Business.Interfaces;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Demo.Business.Notifications
 {
-    class Notifier
+    public class Notifier : INotifier
     {
+        private List<Notification> _notifications;
+
+        public Notifier()
+        {
+            _notifications = new List<Notification>();
+        }
+
+        public List<Notification> GetNotifications()
+        {
+            return _notifications;
+        }
+
+        public void Handle(Notification notification)
+        {
+            _notifications.Add(notification);
+        }
+
+        public bool HasNotification()
+        {
+            return _notifications.Any();
+        }
     }
 }
