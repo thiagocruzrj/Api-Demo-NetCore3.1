8b36c93ca70440b0755beb8fdf78290c368cd071
Thiago Cruz <thagocruz@gmail.com>
2020-04-12 13:32:31 -0300

    Create supplier entity class

---
+++ SalesSystem/src/SalesSystem.Business/Models/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesSystem.Business.Models
+{
+    public class Fornecedor : Entity
+    {
+        public string Nome { get; set; }
+        public string Documento { get; set; }
+        public TipoFornecedor TipoFornecedor { get; set; }
+        public Endereco Endereco { get; set; }
+        public bool Ativo { get; set; }
+
+        // Ef. Relation
+        public IEnumerable<Produto> Produtos { get; set; }
+    }
+}
