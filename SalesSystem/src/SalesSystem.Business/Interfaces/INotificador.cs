b851307babad285f77c2745b4703a59517dcf12d
Thiago Cruz <thagocruz@gmail.com>
2020-04-12 18:07:21 -0300

    Add INotificador interface

---
+++ SalesSystem/src/SalesSystem.Business/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using SalesSystem.Business.Notifications;
+using System.Collections.Generic;
+
+namespace SalesSystem.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
