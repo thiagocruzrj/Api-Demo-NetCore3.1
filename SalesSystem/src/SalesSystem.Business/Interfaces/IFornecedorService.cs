25c9a36e21d8d0e1beaf759c94c08a72e9921b3f
Thiago Cruz <thagocruz@gmail.com>
2020-04-12 18:15:57 -0300

    Add IFornecedorService intereface

---
+++ SalesSystem/src/SalesSystem.Business/Interfaces/IFornecedorService.cs
@@ -0,0 +1,14 @@
+﻿using SalesSystem.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Business.Interfaces
+{
+    public interface IFornecedorService : IDisposable
+    {
+        Task<bool> Adicionar(Fornecedor fornecedor);
+        Task<bool> Atualizar(Fornecedor fornecedor);
+        Task<bool> Remover(Guid id);
+        Task AtualizarEndereco (Endereco endereco);
+    }
+}
