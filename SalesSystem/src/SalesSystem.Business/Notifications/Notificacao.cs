b359eb8cc0a3685bb4e0fe645426135c0f70d1f6
Thiago Cruz <thagocruz@gmail.com>
2020-04-12 16:55:23 -0300

    Add notification class

---
+++ SalesSystem/src/SalesSystem.Business/Notifications/Notificacao.cs
@@ -0,0 +1,12 @@
+﻿namespace SalesSystem.Business.Notifications
+{
+    public class Notificacao
+    {
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+
+        public string Mensagem { get; }
+    }
+}
