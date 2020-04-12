ade7844d6455315ef22dee44987956cc00042e29
Thiago Cruz <thagocruz@gmail.com>
2020-04-12 18:15:19 -0300

    Add IFornecedorService intereface

---
+++ SalesSystem/src/SalesSystem.Business/Interfaces/IFornecedorRepository.cs
@@ -0,0 +1,12 @@
+﻿using SalesSystem.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Business.Interfaces
+{
+    public interface IFornecedorRepository : IRepository<Fornecedor>
+    {
+        Task<Fornecedor> ObterFornecedorEndereco(Guid id);
+        Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id);
+    }
+}
