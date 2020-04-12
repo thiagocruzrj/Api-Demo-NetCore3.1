d6da3a09e5850fca8837408aa08a6abf22817e10
Thiago Cruz <thagocruz@gmail.com>
2020-04-12 18:08:49 -0300

    Add IEnderecoRepository intereface

---
+++ SalesSystem/src/SalesSystem.Business/Interfaces/IEnderecoRepository.cs
@@ -0,0 +1,11 @@
+﻿using SalesSystem.Business.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Business.Interfaces
+{
+    public interface IEnderecoRepository : IRepository<Endereco>
+    {
+        Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId);
+    }
+}
