d361fb3b62a736a93c618df205879257f2bcd5f5
Thiago Cruz <thagocruz@gmail.com>
2020-04-12 22:51:51 -0300

    Starting dbcontext

---
+++ SalesSystem/src/SalesSystem.Data/Context/MeuDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesSystem.Business.Models;
+
+namespace SalesSystem.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
