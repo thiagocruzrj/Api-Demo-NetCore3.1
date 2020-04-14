c9e938f903108e5536f74c3913530e908989e2fb
Thiago Cruz <thagocruz@gmail.com>
2020-04-13 22:05:25 -0300

    Implement fornecedor repo

--- SalesSystem/src/SalesSystem.Data/Repository/FornecedorRepository.cs
+++ SalesSystem/src/SalesSystem.Data/Repository/FornecedorRepository.cs
@@ -1,10 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesSystem.Business.Interfaces;
+using SalesSystem.Business.Models;
+using SalesSystem.Data.Context;
+using System;
+using System.Threading.Tasks;
 
 namespace SalesSystem.Data.Repository
 {
-    class FornecedorRepository
+    public class FornecedorRepository : Repository<Fornecedor>, IFornecedorRepository
     {
+        public FornecedorRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Fornecedor> ObterFornecedorEndereco(Guid id)
+        {
+            return await Db.Fornecedores.AsNoTracking()
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task<Fornecedor> ObterFornecedorProdutosEndereco(Guid id)
+        {
+            System.Linq.IQueryable<Fornecedor> queryable = Db.Fornecedores.AsNoTracking();
+            return await queryable
+                .Include(c => c.Produtos)
+                .Include(c => c.Endereco)
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
     }
 }
