1f7481839cb69a6a9750cba59d8231fae1236355
Thiago Cruz <thagocruz@gmail.com>
2020-04-13 22:01:05 -0300

    Add EnderecoRepo

---
+++ SalesSystem/src/SalesSystem.Data/Repository/EnderecoRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesSystem.Business.Interfaces;
+using SalesSystem.Business.Models;
+using SalesSystem.Data.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Data.Repository
+{
+    public class EnderecoRepository : Repository<Endereco>, IEnderecoRepository
+    {
+        public EnderecoRepository(MeuDbContext context) : base(context) { }
+
+        public async Task<Endereco> ObterEnderecoPorFornecedor(Guid fornecedorId)
+        {
+            return await Db.Enderecos.AsNoTracking()
+                .FirstOrDefaultAsync(f => f.FornecedorId == fornecedorId);
+        }
+    }
+}
---
+++ SalesSystem/src/SalesSystem.Data/Repository/FornecedorRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SalesSystem.Data.Repository
+{
+    class FornecedorRepository
+    {
+    }
+}
