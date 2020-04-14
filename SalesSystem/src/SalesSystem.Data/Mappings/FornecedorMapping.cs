203403d982affc7c13185ce9de11a40972def5fa
Thiago Cruz <thagocruz@gmail.com>
2020-04-13 21:45:05 -0300

    Implement FornecedorMapping

--- SalesSystem/src/SalesSystem.Data/Mappings/FornecedorMapping.cs
+++ SalesSystem/src/SalesSystem.Data/Mappings/FornecedorMapping.cs
@@ -1,10 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SalesSystem.Business.Models;
 
 namespace SalesSystem.Data.Mappings
 {
-    class FornecedorMapping
+    public class FornecedorMapping : IEntityTypeConfiguration<Fornecedor>
     {
+        public void Configure(EntityTypeBuilder<Fornecedor> builder)
+        {
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(200)");
+
+            builder.Property(p => p.Documento)
+                .IsRequired()
+                .HasColumnType("varchar(14)");
+
+            // 1 : 1 => Fornecedor : Endereco
+            builder.HasOne(f => f.Endereco)
+                .WithOne(e => e.Fornecedor);
+
+            // 1 : N => Fornecedor : Produtos
+            builder.HasMany(f => f.Produtos)
+                .WithOne(p => p.Fornecedor)
+                .HasForeignKey(p => p.FornecedorId);
+
+            builder.ToTable("Fornecedores");
+        }
     }
 }
