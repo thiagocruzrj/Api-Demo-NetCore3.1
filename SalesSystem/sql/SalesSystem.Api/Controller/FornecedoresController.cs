e71f746d57c4fb06ecbacb8284bfdcd0bea558b0
Thiago Cruz <thagocruz@gmail.com>
2020-04-14 21:19:04 -0300

    Create add fornecedor method

--- SalesSystem/sql/SalesSystem.Api/Controller/FornecedoresController.cs
+++ SalesSystem/sql/SalesSystem.Api/Controller/FornecedoresController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using SalesSystem.Api.ViewModels;
 using SalesSystem.Business.Interfaces;
+using SalesSystem.Business.Models;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
@@ -12,22 +13,40 @@ namespace SalesSystem.Api.Controller
     public class FornecedoresController : MainController
     {
         private readonly IFornecedorRepository _fornecedorRepository;
+        private readonly IFornecedorService _fornecedorService;
         private readonly IMapper _mapper;
 
-        public FornecedoresController(IFornecedorRepository fornecedorRepository, IMapper mapper)
+        public FornecedoresController(IFornecedorRepository fornecedorRepository,
+                                        IMapper mapper, 
+                                        IFornecedorService fornecedorService)
         {
             _fornecedorRepository = fornecedorRepository;
             _mapper = mapper;
+            _fornecedorService = fornecedorService;
         }
 
+        [HttpGet]
         public async Task<IEnumerable<FornecedorViewModel>> ObterTodos()
         {
             return _mapper.Map<IEnumerable<FornecedorViewModel>>(await _fornecedorRepository.ObterTodos());
         }
 
-        public async Task<FornecedorViewModel> ObterPorId(Guid id)
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorViewModel>> ObterPorId(Guid id)
         {
-            return await ObterFornecedorProdutosEndereco(id);
+            var fornecedor = await ObterFornecedorProdutosEndereco(id);
+            if (fornecedor == null) return NotFound();
+            return fornecedor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorViewModel>> AdicionarFornecedor(FornecedorViewModel fornecedorViewModel)
+        {
+            if (!ModelState.IsValid) return BadRequest();
+            var fornecedor = _mapper.Map<Fornecedor>(fornecedorViewModel);
+            await _fornecedorService.Adicionar(fornecedor);
+
+            return Ok(fornecedor);
         }
 
         public async Task<FornecedorViewModel> ObterFornecedorProdutosEndereco(Guid id)
