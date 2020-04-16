790c93b077fbe6a08e47ac00fe95490e4aa45e95
Thiago Cruz <thagocruz@gmail.com>
2020-04-15 23:31:19 -0300

    Add GetAll products

---
+++ SalesSystem/sql/SalesSystem.Api/Controller/ProdutosController.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SalesSystem.Api.ViewModels;
+using SalesSystem.Business.Interfaces;
+using SalesSystem.Business.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SalesSystem.Api.Controller
+{
+    [Route("api/produtos")]
+    public class ProdutosController : MainController
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+        public ProdutosController(INotificador notificador,
+            IProdutoRepository produtoRepository,
+            IProdutoService produtoService,
+            IMapper mapper) : base(notificador)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
+        {
+            return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores());
+        }
+    }
+}
