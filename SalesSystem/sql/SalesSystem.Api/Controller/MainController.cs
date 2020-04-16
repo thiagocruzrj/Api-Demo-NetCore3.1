07891367dbcd429532c93a287d7fb1322f8e35e6
Thiago Cruz <thagocruz@gmail.com>
2020-04-15 21:48:07 -0300

    Notify all invalid cases

--- SalesSystem/sql/SalesSystem.Api/Controller/MainController.cs
+++ SalesSystem/sql/SalesSystem.Api/Controller/MainController.cs
@@ -1,10 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using SalesSystem.Business.Interfaces;
+using SalesSystem.Business.Notifications;
+using System;
+using System.Linq;
 
 namespace SalesSystem.Api.Controller
 {
     [ApiController]
     public abstract class MainController : ControllerBase
     {
+        private readonly INotificador _notificador;
 
+        protected MainController(INotificador notificador)
+        {
+            _notificador = notificador;
+        }
+
+        protected ActionResult CustomResponse(ModelStateDictionary modelState)
+        {
+
+        }
+
+        protected void NotificarErrorModelStateInvalida(ModelStateDictionary modelState)
+        {
+            var erros = modelState.Values.SelectMany(e => e.Errors);
+            foreach (var erro in erros)
+            {
+                var erroMessage = erro.Exception == null ? erro.ErrorMessage : erro.Exception.Message;
+                NotificarErro(erroMessage);
+            }
+        }
+
+        protected void NotificarErro(string mensagem)
+        {
+            _notificador.Handle(new Notificacao(mensagem));
+        }
     }
 }
