8a361f289ebfcb2249bb31a940937f7b2a386b81
Thiago Cruz <thagocruz@gmail.com>
2020-04-15 23:33:52 -0300

    Add ObterPorId

--- SalesSystem/sql/SalesSystem.Api/Controller/ProdutosController.cs
+++ SalesSystem/sql/SalesSystem.Api/Controller/ProdutosController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SalesSystem.Api.ViewModels;
 using SalesSystem.Business.Interfaces;
 using SalesSystem.Business.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -28,6 +29,14 @@ namespace SalesSystem.Api.Controller
         public async Task<IEnumerable<ProdutoViewModel>> ObterTodos()
         {
             return _mapper.Map<IEnumerable<ProdutoViewModel>>(await _produtoRepository.ObterProdutosFornecedores());
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoViewModel>> ObterPorId(Guid id)
+        {
+            var produtoViewModel = await ObterPorId(id);
+            if (produtoViewModel == null) return NotFound();
+            return produtoViewModel;
         }
     }
 }
