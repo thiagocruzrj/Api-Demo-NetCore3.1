f3185519a53db9d78b11e0291ab8ac00687c3ac9
Thiago Cruz <thagocruz@gmail.com>
2020-04-16 22:11:42 -0300

    Create config api class

---
+++ SalesSystem/sql/SalesSystem.Api/Configuration/ApiConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SalesSystem.Api.Configuration
+{
+    public static class ApiConfig
+    {
+        public static IServiceCollection WebConfig(this IServiceCollection services)
+        {
+            return services;
+        }
+
+        public static IApplicationBuilder UseMvcConfiguration(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
