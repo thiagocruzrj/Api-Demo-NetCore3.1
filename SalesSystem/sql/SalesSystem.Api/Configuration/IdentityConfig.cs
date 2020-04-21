ccc5b3982869800c04ef19f219c60c6dc34343f3
Thiago Cruz <thagocruz@gmail.com>
2020-04-20 21:42:56 -0300

    Add config schemes

--- SalesSystem/sql/SalesSystem.Api/Configuration/IdentityConfig.cs
+++ SalesSystem/sql/SalesSystem.Api/Configuration/IdentityConfig.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SalesSystem.Api.Data;
 using SalesSystem.Api.Extensions;
+using System.Text;
 
 namespace SalesSystem.Api.Configuration
 {
@@ -20,6 +22,19 @@ namespace SalesSystem.Api.Configuration
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddErrorDescriber<IdentityMensagensPortugues>()
                 .AddDefaultTokenProviders();
+
+            // JWT
+            var appSettingsSection = configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            });
 
             return services;
         }
