0aa593bbc62fcee6ac1c152d95524f23be1290b5
Thiago Cruz <thagocruz@gmail.com>
2020-04-25 14:57:37 -0300

    Implement filter to claim request

--- SalesSystem/sql/SalesSystem.Api/Extensions/CustomAuthorize.cs
+++ SalesSystem/sql/SalesSystem.Api/Extensions/CustomAuthorize.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
 using System;
 using System.Linq;
 using System.Security.Claims;
@@ -20,6 +21,30 @@ namespace SalesSystem.Api.Extensions
         public ClaimsAuthorizeAttribute(string claimName, string claimValue) : base(typeof(RequisitoClaimFilter))
         {
             Arguments = new object[] { new Claim(claimName, claimValue) };
+        }
+    }
+
+    public class RequisitoClaimFilter : IAuthorizationFilter
+    {
+        private readonly Claim _claim;
+
+        public RequisitoClaimFilter(Claim claim)
+        {
+            _claim = claim;
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (!context.HttpContext.User.Identity.IsAuthenticated)
+            {
+                context.Result = new StatusCodeResult(401);
+                return;
+            }
+
+            if(!CustomAuthorization.ValidarClaimsUsuario(context.HttpContext, _claim.Type, _claim.Value))
+            {
+                context.Result = new StatusCodeResult(403);
+            }
         }
     }
 }
