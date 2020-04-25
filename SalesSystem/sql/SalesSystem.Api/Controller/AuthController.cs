94f97a561e764ae1d76591933abeb85ff3fa66bb
Thiago Cruz <thagocruz@gmail.com>
2020-04-25 15:33:59 -0300

    Add LoginResponseViewModel on GetJwt method

--- SalesSystem/sql/SalesSystem.Api/Controller/AuthController.cs
+++ SalesSystem/sql/SalesSystem.Api/Controller/AuthController.cs
@@ -7,6 +7,7 @@ using SalesSystem.Api.ViewModels;
 using SalesSystem.Business.Interfaces;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -78,7 +79,7 @@ namespace SalesSystem.Api.Controller
             return CustomResponse(loginUser);
         }
 
-        private async Task<string> GerarJwt(string email)
+        private async Task<LoginResponseViewModel> GerarJwt(string email)
         {
             var user = await _userManager.FindByEmailAsync(email);
             var claims = await _userManager.GetClaimsAsync(user);
@@ -103,13 +104,28 @@ namespace SalesSystem.Api.Controller
             {
                 Issuer = _appSettings.Emissor,
                 Audience = _appSettings.ValidoEm,
+                Subject = identityClaims,
                 Expires = DateTime.UtcNow.AddHours(_appSettings.ExpiracaoHoras),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
             });
 
             var encodedToken = tokenHandler.WriteToken(token);
-            return encodedToken;
+
+            var response = new LoginResponseViewModel
+            {
+                AccessToken = encodedToken,
+                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpiracaoHoras).TotalSeconds,
+                UserToken = new UserTokenViewModel
+                {
+                    Id = user.Id,
+                    Email = user.Email,
+                    Claims = claims.Select(c => new ClaimViewModel { Type = c.Type, Value = c.Value })
+                }
+            };
+
+            return response;
         }
+
         private static long ToUnixEpochDate(DateTime date)
             => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
     }
--- SalesSystem/sql/SalesSystem.Api/ViewModels/UserViewModel.cs
+++ SalesSystem/sql/SalesSystem.Api/ViewModels/UserViewModel.cs
@@ -28,6 +28,13 @@ namespace SalesSystem.Api.ViewModels
         public string Password { get; set; }
     }
 
+    public class LoginResponseViewModel
+    {
+        public string AccessToken { get; set; }
+        public double ExpiresIn { get; set; }
+        public UserTokenViewModel UserToken { get; set; }
+    }
+
     public class UserTokenViewModel
     {
         public string Id { get; set; }
