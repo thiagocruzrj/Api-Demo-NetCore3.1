0ed5b94856700d876b5718dd6b88984e9458324a
Thiago Cruz <thagocruz@gmail.com>
2020-04-26 00:06:20 -0300

    Add test api

---
+++ SalesSystem/sql/SalesSystem.Api/V1/Controller/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesSystem.Api.Controllers;
+using SalesSystem.Business.Interfaces;
+
+namespace SalesSystem.Api.V1.Controller
+{
+    [ApiVersion("1.0", Deprecated = true)]
+    [Route("api/v{version:apiVersion}/teste")]
+    public class TesteController : MainController
+    {
+
+        public TesteController(INotificador notificador, IUser appUser) : base(notificador, appUser) { }
+
+        [HttpGet]
+        public string Valor()
+        {
+            return "Sou a V1";
+        }
+    }
+}
