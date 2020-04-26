9c3bd9afdb4abb3d38cb5dc7eafaf463872f2695
Thiago Cruz <thagocruz@gmail.com>
2020-04-26 14:17:24 -0300

    Add default values

--- SalesSystem/sql/SalesSystem.Api/Configuration/SwaggerConfig.cs
+++ SalesSystem/sql/SalesSystem.Api/Configuration/SwaggerConfig.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
+using System.Linq;
 
 namespace SalesSystem.Api.Configuration
 {
@@ -38,6 +40,45 @@ namespace SalesSystem.Api.Configuration
             }
 
             return info;
+        }
+    }
+
+    public class SwaggerDefaultValues : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+            {
+                return;
+            }
+
+            foreach (var parameter in operation.Parameters)
+            {
+                var description = context.ApiDescription
+                    .ParameterDescriptions
+                    .First(p => p.Name == parameter.Name);
+
+                var routeInfo = description.RouteInfo;
+
+                operation.Deprecated = OpenApiOperation.DeprecatedDefault;
+
+                if (parameter.Description == null)
+                {
+                    parameter.Description = description.ModelMetadata?.Description;
+                }
+
+                if (routeInfo == null)
+                {
+                    continue;
+                }
+
+                if (parameter.In != ParameterLocation.Path && parameter.Schema.Default == null)
+                {
+                    parameter.Schema.Default = new OpenApiString(routeInfo.DefaultValue.ToString());
+                }
+
+                parameter.Required |= !routeInfo.IsOptional;
+            }
         }
     }
 }
