06b807eba4207a76a56bd9638a056720c3552cd9
Thiago Cruz <thagocruz@gmail.com>
2020-04-26 14:18:39 -0300

    Add token info type

--- SalesSystem/sql/SalesSystem.Api/Configuration/SwaggerConfig.cs
+++ SalesSystem/sql/SalesSystem.Api/Configuration/SwaggerConfig.cs
@@ -11,6 +11,41 @@ namespace SalesSystem.Api.Configuration
 {
     public static class SwaggerConfig
     {
+        public static IServiceCollection AddSwaggerConfig(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.OperationFilter<SwaggerDefaultValues>();
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
+            });
+
+            return services;
+        }
+
         public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
         {
             readonly IApiVersionDescriptionProvider provider;
